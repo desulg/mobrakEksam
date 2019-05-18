e84b58e2d64a07983a3b46d445d6e9fce64c7649
Dennis Richard Shulga <desulg@tlu.ee>
2019-05-18 15:18:38 +0300

    esialgne

---
+++ Game Project/Assets/Scripts/ghostScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class ghostScript : MonoBehaviour {
+
+    public GameObject target; //this is the player
+
+    NavMeshAgent agent; //This is a reference for the ghost
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        if (target == null)
+            target = GameObject.FindGameObjectWithTag("Player");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //this is for updating the target location
+        agent.destination = target.transform.position;
+
+	}
+    //function to detect when the ghost gets the player
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene("menu");
+    }
+}
---
+++ Game Project/Assets/Scripts/nextLevelScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextLevelScript : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //load the next level
+    }
+}
